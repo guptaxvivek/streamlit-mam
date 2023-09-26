c0a8a328bf8e98a9df8937d4580f2088f673d279
guptaxvivek <gupta.vi2001@gmail.com>
2023-09-26 22:07:14 +0530

    bug fix

--- main.py
+++ main.py
@@ -141,7 +141,10 @@ if start:
                 st.title(f"{i}. last Click")
                 attribution_last_click = attributions.attribution_last_click()
                 df = attribution_last_click[1]
-                fig = px.pie(df, values=df.columns[1], names=df.columns[0])
+                try:
+                    fig = px.pie(df, values=df.columns[1], names=df.columns[0])
+                except AttributeError:
+                    fig = px.pie(values=df.values, names=df.index)
                 st.plotly_chart(fig, use_container_width=True)
                 st.write("The Last Click heuristic, also known as the Last Touch attribution model, is one of the simplest and most widely used methods in marketing attribution models. This model attributes 100% of the conversion value to the last touchpoint or interaction that the customer had before converting, like making a purchase or filling out a form.")
 
@@ -151,7 +154,10 @@ if start:
                 st.title(f"{i}. First Click")
                 attribution_first_click = attributions.attribution_first_click()
                 df = attribution_first_click[1]
-                fig = px.pie(df, values=df.columns[1], names=df.columns[0])
+                try:
+                    fig = px.pie(df, values=df.columns[1], names=df.columns[0])
+                except AttributeError:
+                    fig = px.pie(values=df.values, names=df.index)
                 st.plotly_chart(fig, use_container_width=True)
                 st.write("The First Click model, sometimes also known as the First Touch model, is a heuristic (or rule of thumb) that attributes 100% of the conversion value to the first interaction or touchpoint a customer has with a brand or campaign. This means that if a conversion occurs, the entire credit for that conversion is given to the first channel through which the user engaged with the brand.")
 
@@ -160,7 +166,10 @@ if start:
             with c[i-1]:
                 st.title(f"{i}. Last Click Non Direct Model")
                 data = attributions.attribution_last_click_non(but_not_this_channel='Direct')[1]
-                fig = px.bar(data, x='channels', y=data.columns[1])
+                try:
+                    fig = px.bar(data, x='channels', y=data.columns[1])
+                except AttributeError:
+                    fig = px.bar(x=data.index, y=data.values)
                 st.plotly_chart(fig, use_container_width=True)
                 st.write("In this, Direct traffic is overwritten in case previous interations have a specific traffic source other than Direct itself in a given timespan (6 months by default).")
         if pos_model:
@@ -191,7 +200,10 @@ if start:
                     df = attributions.attribution_time_decay(
                         decay_over_time=0.6,
                         frequency=7)[1]
-                fig = px.pie(df, values=df.columns[1], names=df.channels)
+                try:
+                    fig = px.pie(df, values=df.columns[1], names=df.channels)
+                except AttributeError:
+                    fig = px.pie(values=df.values, names=df.index)
                 st.plotly_chart(fig, use_container_width=True)
                 st.write("The Time Decay model in marketing attribution is a way to assign credit to the different interactions, or touchpoints, a customer has with your marketing channels before they make a purchase or conversion. In this model, the interactions that occur closer to the time of conversion receive more credit than those that occur further away in time. Essentially, the credit decays as you go back in time from the conversion.")
 
@@ -200,7 +212,6 @@ if start:
             with c[i-1]:
                 st.title(f"{i}. Attribution Using Markov")
                 attribution_markov = attributions.attribution_markov(transition_to_same_state=False)
-                # ax, fig = plt.subplots(figsize=(15, 10))
                 fig = px.imshow(attribution_markov[2].round(3), text_auto=True)
                 st.plotly_chart(fig, use_container_width=True)
                 if removal_effect:
